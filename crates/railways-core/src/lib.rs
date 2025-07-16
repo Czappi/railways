9ef6c0a6577ffef6d4ba07b5f3d6f2f19222e490
Czappi <38006928+Czappi@users.noreply.github.com>
2025-07-17 00:12:41 +0200

    document some ideas
    to not forget them then think of them again in a months time :D

--- crates/railways-core/src/lib.rs
+++ crates/railways-core/src/lib.rs
@@ -2,12 +2,30 @@ pub mod dsl;
 //pub mod types;
 
 mod plans {
+    use crate::dsl::{LogicalNode, Node};
 
-    struct TestStateManager;
+    /// - can be run multiple lines.
+    /// - made of [Node]s
+    /// - can act like a node
+    struct Pipeline;
 
-    impl TestStateManager {
-        fn a(&self, field: String) -> Result<String, ()> {
-            Err(())
-        }
+    /// parallel primitive
+    fn join<'a, T: LogicalNode<'a> + ?Sized>(
+        // Main "thread", which will be returned
+        main: Node<'a, T>,
+        // other "threads", which will be joined to the "main thread"
+        parallels: &'a [Node<'a, dyn LogicalNode<'a>>],
+    ) {
+        todo!()
+    }
+
+    /// joines the nodes together into a tuple
+    /// so it can return boths values
+    fn tuple<'a, T0, T1>(tuple: (T0, T1))
+    where
+        T0: LogicalNode<'a> + Sized,
+        T1: LogicalNode<'a> + Sized,
+    {
+        todo!()
     }
 }
